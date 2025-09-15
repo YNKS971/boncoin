5e6f25c95c52ee3886c11a9293abc16bd2e6cd8a
YNKS971 <dylan.pqt@gmail.com>
2025-09-15 07:57:50 +0200

    jsp

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -11,29 +11,67 @@ class UserController{
 $regName = "/^[a-zA-Zàèé\-]+$/";
 
 // Je ne lance qu'uniquement lorsqu'il y a un formulaire validée via la méthod POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // je créé un tableau d'erreurs vide car pas d'erreur
-    $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+            // je créé un tableau d'erreurs vide car pas d'erreur
+            $errors = [];
 
-    if (isset($_POST["email"])) {
-        // on va vérifier si c'est vide
-        if (empty($_POST["email"])) {
-            // si c'est vide, je créé une erreur dans mon tableau
-            $errors['email'] = 'Mail obligatoire';
-        } else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-            // si mail non valide, on créé une erreur
-            $errors['email'] = 'Mail non valide';
+            if (isset($_POST["username"])) {
+                // on va vérifier si c'est vide
+                if (empty($_POST["username"])) {
+                    // si c'est vide, je créé une erreur dans mon tableau
+                    $errors['username'] = 'Pseudo obligatoire';
+                } else if (User::checkUsername($_POST["username"])) {
+                    // si le pseudo déjà présent dans notre bdd, on créé un message d'erreur
+                    $errors['username'] = 'Pseudo déjà utilisé';
+                }
+            }
+
+            if (isset($_POST["email"])) {
+                // on va vérifier si c'est vide
+                if (empty($_POST["email"])) {
+                    // si c'est vide, je créé une erreur dans mon tableau
+                    $errors['email'] = 'Mail obligatoire';
+                } else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                    // si mail non valide, on créé une erreur
+                    $errors['email'] = 'Mail non valide';
+                } else if (User::checkMail($_POST["email"])) {
+                    // si mail déjà utilisé, on créé un message d'erreur dans notre tableau
+                    $errors['email'] = 'Mail déjà utilisé';
+                }
+            }
+
+            if (isset($_POST["password"])) {
+                // on va vérifier si c'est vide
+                if (empty($_POST["password"])) {
+                    // si c'est vide, je créé une erreur dans mon tableau
+                    $errors['password'] = 'Mot de passe obligatoire';
+                } else if (strlen($_POST["password"]) < 8) {
+                    // si le mot de passe est trop court, on créé une erreur
+                    $errors['password'] = 'Mot de passe trop court (minimum 8 caractères)';
+                }
+            }
+
+            if (isset($_POST["confirmPassword"])) {
+                // on va vérifier si c'est vide
+                if (empty($_POST["confirmPassword"])) {
+                    // si c'est vide, je créé une erreur dans mon tableau
+                    $errors['confirmPassword'] = 'Confirmation du mot de passe obligatoire';
+                } else if ($_POST["confirmPassword"] !== $_POST["password"]) {
+                    // si les deux mots de passe ne sont pas identiques, on créé une erreur
+                    $errors['confirmPassword'] = 'Les mots de passe ne sont pas identiques';
+                }
+            }
+
+            if (!isset($_POST["cgu"])) {
+                // si la case n'est pas cochée, on créé une erreur
+                $errors['cgu'] = 'Vous devez accepter les CGU';
+            }
         }
-    }
 
-
-    if (empty($errors)) {
-        // on inclut un paramètre URL pour pouvoir l'utiliser dans la page confirmation.php
-        header("Location: confirmation.php?email=" . $_POST["email"]);
+        require_once __DIR__ . "/../Views/register.php";
     }
-}
 
     /**
      * PERMET DE RETOURNER au registre
@@ -73,4 +111,3 @@ header('Location: home.php');
 
 }
 
-}
--- src/views/page404.php
+++ src/views/page404.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
+
+
+<div class="row m-0">
+        <div class="col-6">
+            <img src=" " alt="erreur 404" class="img-fluid">
+        </div>
+        <div class="col-6 text-center d-flex flex-column justify-content-center">
+            <p class="h3">La page demandée n'existe pas</p>
+            <div>
+                <a href="index.php" class="btn btn-lg btn-light col-4 mt-3">Home</a>
+            </div>
+        </div>
+    </div>
+    
     
 </body>
 </html>
